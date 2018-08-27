cc885235397dfffc58b994d6fc11e8f298982ebe
afcentry <afcentry@163.com>
2018-08-27 12:00:03 +0800

    book增加

---
+++ BaiDu/main.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+"""
+Author:afcentry
+DateTime:2018/08/24
+Describe:通過python模擬網頁請求實現百度搜索引擎，輸入：關鍵字  輸出：返回搜索結果標題列表以及對應URL
+"""
+
+import requests
+
+class BaiDuSearch:
+
+    def __init__(self):
+        pass
